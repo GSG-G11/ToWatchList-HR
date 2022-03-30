c70621f8400fb948372afd185f318e558a29716d
HaniOlwan <heenoow@gmail.com>
2022-03-30 16:34:06 +0300

    handle input submit in form page with alert message on book submit Relates #5

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,22 +1,40 @@
 import {Component} from 'react';
 class Form extends Component {
-    // state = {  } 
-    render() { 
+    
+    state = {  
+        title:''
+    } 
+
+    inputHandler(event){
+        const title = event.target.value;
+
+        this.setState({
+            title
+        });
+    }
+
+    submitHandler(event){
+        event.preventDefault();
+        alert('New Book Sumbited');
+    }
+    render() {
         return (
-            <section className="header">
+        <section className="header">
             <div className="form">
                 <h1 className="form_title">Add your Movie</h1>
                 <div className="form_content"> 
-                <input type="text" name="title" placeholder='Add your movie'/>
-                <input type="text" name="title" placeholder='Add your movie'/>
-                <button className="button" type="button">Add Movie</button>
+                <form method="get" onSubmit={this.submitHandler}>
+                <input type="text" name="title" placeholder='Add your movie' onChange={this.inputHandler.bind(this)}/>
+                <button className="button" type="submit">Add Movie</button>
+                </form>
                 </div>
             </div>
+
             <div className="form">
                 <h1 className="form_title">Add your Movie</h1>
                 <div className="form_content"> 
-                <input type="text" name="title" placeholder='Add your movie'/>
-                <input type="text" name="title" placeholder='Add your movie'/>
+                <input type="text"  placeholder='Add your movie'/>
+                <input type="text"  placeholder='Add your movie'/>
                 <button className="button" type="button">Add Movie</button>
                 </div>
             </div>
